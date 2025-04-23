04f1a2fbd24e118da81f98f15a97f1e7907c9498
sunjae0902 <sunjaegg0902@naver.com>
2025-04-23 23:18:19 +0900

    update: 네비게이팅

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link"; // 네비게이팅
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push('/search'); // replace, back 등 지원
+  };
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>index</Link> 
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/books/1"}>books/1</Link>
+        <div>
+          <button onClick={onClickButton}>search 페이지로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
