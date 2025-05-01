3cd277b4911564b023d032eb04153fa40776ca1a
sunjae0902 <sunjaegg0902@naver.com>
2025-05-01 21:04:30 +0900

    update: SSR practice

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,36 +1,35 @@
 import SearchableLayout from '@/components/search/searchable-layout';
 import style from './index.module.css';
 import { ReactNode, useEffect } from 'react';
-import books from '@/mock/books.json';
 import BookItem from '@/components/books/boot-item';
 import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/main/fetch-books';
+import fetchRandomBooks from '@/lib/main/fetch-random-books';
 
-export const getServerSideProps = () => { // SSR
-  // 접속 요청 시, 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 가져오는 함수 -> 서버 측에서 한 번만 실행됨
-  const data = "hello";
-  console.log("서버 환경에서만 실행됨");
+export const getServerSideProps = async () => { // SSR
+  const [bookList, recommendedBookList] = await Promise.all([ // 여러 비동기 함수 동시에 호출!!
+    fetchBooks(),
+    fetchRandomBooks()
+  ]);
+
   return {
-    props: {data} // 반드시 props를 포함하는 객체를 리턴해야 함
+    props: {
+      bookList,
+      recommendedBookList
+    }
   };
-
 };
 
-export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) { // 메서드에서 props 받아옴
-  console.log(data);
-
-  useEffect(() => { // 브라우저에서만 렌더링 될 때마다 실행됨
-    console.log(window);
-  });
-
+export default function Home({ bookList, recommendedBookList }: InferGetServerSidePropsType<typeof getServerSideProps>) { // 메서드에서 props 받아옴
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => <BookItem key = {book.id} {...book}/>)}
+        {recommendedBookList.map((book) => <BookItem key = {book.id} {...book}/>)}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => <BookItem key={book.id} {...book} />)}
+        {bookList.map((book) => <BookItem key={book.id} {...book} />)}
       </section>
     </div>
   );
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,17 +1,27 @@
 import SearchableLayout from "@/components/search/searchable-layout";
 import { useRouter } from "next/router"; // 쿼리 파라미터 불러오기 위한 훅
 import { ReactNode } from "react";
-import books from '@/mock/books.json';
 import BookItem from "@/components/books/boot-item";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/main/fetch-books";
 
-export default function SearchPage() {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+    const searchedBookList = await fetchBooks(context.query.q as string);
 
+    return {
+        props: {
+            searchedBookList
+        }
+    }
+}
+
+export default function SearchPage({searchedBookList} : InferGetServerSidePropsType<typeof getServerSideProps>) {
     const router = useRouter();
     console.log(router); // 라우터 객체 정보 출력 (2번 렌더링 되므로 2번 출력됨)
     const { q } = router.query; // router.query 객체 안에 'q'를 추출해서 담음. const q = router.query.q 와 같음
     return (
         <div>
-            {books.map((book) => <BookItem key={book.id} {...book} />)}
+            {searchedBookList.map((book) => <BookItem key={book.id} {...book} />)}
         </div>
     );
 }
