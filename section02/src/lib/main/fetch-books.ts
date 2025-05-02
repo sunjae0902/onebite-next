c7e439e136611e5117cc637b16568f3b8eb28967
sunjae0902 <sunjaegg0902@naver.com>
2025-05-03 01:29:20 +0900

    deploy

--- section02/src/lib/main/fetch-books.ts
+++ section02/src/lib/main/fetch-books.ts
@@ -1,11 +1,11 @@
 import { BookData } from "@/types";
 
 export default async function fetchBooks(q?: string) : Promise<BookData[]> {
-    let url = 'http://localhost:12345/book';
+    let url = 'https://onebite-books-server-main-six-taupe.vercel.app/book';
     if (q) {
         url += `/search?q=${q}`;
     }
-    
+
     try {
         const response = await fetch(url);
         if (!response.ok) {
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -24,7 +24,7 @@ export default function SearchPage({searchedBookList} : InferGetServerSidePropsT
         <>
             <Head>
                 <title>한입북스 - 검색 결과</title>
-                <meta property="og:image" content="/thumnail.png"></meta>
+                <meta property="og:image" content="/thumbnail.png"></meta>
                 <meta property="og:title" content="한입북스 - 검색 결과"></meta>
                 <meta property='og:description' content='한입 북스에 등록된 도서들을 만나보세요.'></meta>
             </Head>
